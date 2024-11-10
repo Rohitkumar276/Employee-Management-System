629c0ff61df9883608b1e08f78569a6706abf6c6
Rohitkumar276 <rohitkumar2761990@gmail.com>
2024-11-10 18:34:57 +0530

    rk

---
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+      <footer className='footer'>
+<span>
+    All Rights reserved 2024 by Rohit Kumar
+</span>
+      </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
